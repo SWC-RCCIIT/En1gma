d3aef9e9ca9f0c921e3eaf03f146cae33f8300bb
khushibhaiya <59822432+khushibhaiya@users.noreply.github.com>
2020-01-20 17:37:30 +0530

    my first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<title> Safe Space </title>
+</head>
+<body>
+
+	<!-- Main Body -->
+
+	<div class="container">
+			<h1><u>
+			<center>SAFE SPACE</center>
+		    </u></h1>
+			<!-- About -->
+			<div class="col-md-9 home">
+				<h3><u> DESCRIPTION </u></h3>
+			<br>
+			 
+            </div>	
+
+			<!-- Login Zone -->
+
+			<div class="col-md-3 loginzone">
+				
+				<h1>Login Zone</h1>
+				<form method="post">
+					<label>User ID</label><br>
+					<input type="text" name="uid">
+				    <br>
+				 
+					<label>Password</label><br>
+					<input type="password" name="password">
+					<br><br>
+					<button class = "button" type="submit" name="submit">Log In</button>
+				</form>
+			</div>
+	</div>
+
+
+</body>
+</html>
